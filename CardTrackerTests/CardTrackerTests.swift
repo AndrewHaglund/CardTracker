2c9d5a553770d5ab23b5e34453f71df26351ab21
Andrew Haglund <git@haglund.app>
2025-03-28 16:06:14 -0500

    Initial Commit

---
+++ CardTrackerTests/CardTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  CardTrackerTests.swift
+//  CardTrackerTests
+//
+//  Created by Andrew Haglund on 3/28/25.
+//
+
+import Testing
+
+struct CardTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
